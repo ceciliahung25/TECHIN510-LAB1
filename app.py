bd8ed0a71410dd979adcef845dcba769e1c73a73
ceciliahung25 <cecihung@uw.edu>
2024-03-26 16:31:28 -0700

    init

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.image('./images/cc.JPG', width=200)
+st.title("👋🏻 Hi, my name is Cecilia!")
+st.caption('A designer who aspires to become an anthropologist.')
+
+
+
+st.subheader('Education', divider='rainbow')
+st.subheader('_Streamlit_ is :blue[cool] :sunglasses:')
+
